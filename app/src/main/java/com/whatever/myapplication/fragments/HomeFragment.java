569bfeade16b6c730ae1e7b81be92cb687511193
ramonbreno <ramonicet@gmail.com>
2018-08-09 16:09:52 -0300

    insert menu bottom in main fraagment

--- app/src/main/java/com/whatever/myapplication/fragments/HomeFragment.java
+++ app/src/main/java/com/whatever/myapplication/fragments/HomeFragment.java
@@ -1,5 +1,8 @@
 package com.whatever.myapplication.fragments;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -7,7 +10,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
-import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,45 +18,44 @@ import android.widget.ImageView;
 
 import com.whatever.myapplication.R;
 import com.whatever.myapplication.activitys.MainActivity;
-import com.whatever.myapplication.fragments.CustomerFragment;
-import com.whatever.myapplication.fragments.PropertiesFragment;
-
-import com.whatever.myapplication.fragments.ScheduleFragment;
 
 import java.util.ArrayList;
 
-public class MainFragment extends Fragment {
+
+public class HomeFragment extends Fragment {
+    private ImageView menu;
     private SectionsPagerAdapter sectionsPagerAdapter;
-    private TabLayout mTabLayout;
-    private int[] icons = {
-            R.drawable.home,
-            R.drawable.search,
-            R.drawable.add,
-            R.drawable.user
-    };
+
     @Override
     public View onCreateView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle savedInstanceState) {
-        View view = layoutInflater.inflate(R.layout.fragment_main, viewGroup, false);
+        View view = layoutInflater.inflate(R.layout.fragment_home, viewGroup, false);
 
         sectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-        sectionsPagerAdapter.addFragment(new HomeFragment(), "");
-        sectionsPagerAdapter.addFragment(new CustomerFragment(), "");
-        sectionsPagerAdapter.addFragment(new PropertiesFragment(), "");
-        sectionsPagerAdapter.addFragment(new ScheduleFragment(), "");
+        sectionsPagerAdapter.addFragment(new ScheduleFragment(), "Schedule");
+        sectionsPagerAdapter.addFragment(new PropertiesFragment(), "Properties");
+        sectionsPagerAdapter.addFragment(new CustomerFragment(), "Costumer");
 
-        ViewPager mViewPager = view.findViewById(R.id.content_main_vp);
+        ViewPager mViewPager = view.findViewById(R.id.content_home);
         mViewPager.setAdapter(sectionsPagerAdapter);
 
-        mTabLayout = view.findViewById(R.id.abas_menu);
+        menu = view.findViewById(R.id.menu);
+        MainActivity mainActivity = new MainActivity();
+        final DrawerLayout drawerLayout = mainActivity.getDrawerLayout();
+
+        menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+
+        TabLayout mTabLayout = view.findViewById(R.id.abas);
         mTabLayout.setupWithViewPager(mViewPager);
-        setIconsTab();
 
         return view;
     }
-    private void setIconsTab(){
-        for(int i = 0; i < 4; i++)
-            mTabLayout.getTabAt(i).setIcon(icons[i]);
-    }
+
+
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
         private final ArrayList<Fragment> fragmentList = new ArrayList<>();
         private final ArrayList<String> titleList = new ArrayList<>();
@@ -74,6 +75,7 @@ public class MainFragment extends Fragment {
         void addFragment(Fragment fragment, String title){
             fragmentList.add(fragment);
             titleList.add(title);
+
         }
         @Override
         public Fragment getItem(int position) {
@@ -92,4 +94,3 @@ public class MainFragment extends Fragment {
         }
     }
 }
-
